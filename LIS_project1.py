c69b9224647d59ead0116f9eb9e046d4b5cfe376
wenr <wenr@student.ethz.ch>
2016-03-17 13:40:08 +0100

    gary branch

--- LIS_project1.py
+++ LIS_project1.py
@@ -2,6 +2,9 @@ import csv
 import numpy as np
 import matplotlib.pylab as plt
 from sklearn import linear_model
+from sklearn.metrics import mean_squared_error
+import sys
+
 
 x_train=[]
 y_train=[]
@@ -30,30 +33,19 @@ with open('train.csv', 'r') as csvfile:
             #print(x_train)
             #    q=q+1
                 
-
+'''
 print("TRAINING:")
 #print(int(len(x_train)/15))
+'''
 x_train=np.reshape(x_train,(int(len(x_train)/15),15))
+'''
 print("X (nxd) with "+str(len(x_train))+" samples: ")
 print(x_train)
 print()
 print("Y with "+str(len(y_train))+" samples: ")
 print(y_train)
 print()
-
-    
-#Ordinary Least Squares
-
-x_train = np.array(x_train, dtype = 'float_')
-y_train = np.array(y_train, dtype = 'float_')
-clf = linear_model.LinearRegression()
-clf.fit(x_train,y_train)
-
-w=clf.coef_
-print("Coefficients of Regression: "+"length: "+str(len(w)))
-print(w)
-print()
-
+'''
 #Open test data
 first_line=True
 x_test=[]
@@ -74,15 +66,68 @@ with open('test.csv', 'r') as csvfile:
             #print(entry)
             #print(x_train)
             #    q=q+1
-x_test=np.reshape(x_test,(int(len(x_test)/15),15))
+x_test=np.reshape(x_test,(int(len(x_test)/15),15))    
+
+
+#Ordinary Least Squares
+
+
+
+x_train = np.array(x_train, dtype = 'float_')
+y_train = np.array(y_train, dtype = 'float_')
+
+#clf = linear_model.Ridge(alpha=0)
+clf = linear_model.LinearRegression()
+clf.fit(x_train,y_train)
+RMSE = sys.float_info.max
+w=clf.coef_
+lam_best=0
+
+
+'''
+for lam in np.arange(0,8.1,0.1):
+		
+	clf = linear_model.Ridge(alpha=lam)
+	#clf = linear_model.Ridge(alpha=lam)
+	#clf = linear_model.LinearRegression()
+	clf.fit(x_train,y_train)
+	var_coef=clf.coef_
+	var_mean=mean_squared_error(y_train, np.dot(x_train,var_coef))**0.5	
+	if  var_mean<RMSE:
+		RMSE=var_mean
+		w=var_coef
+		lam_best=lam
+		
+
+#print("Coefficients of Regression: "+"length: "+str(len(w)))
+'''
+
+print("Coefficients of Ridge Regression: "+"length: "+str(len(w)))
+
+print(w)
+print()
+
+
+'''
 print("TEST SET X (kxd) with "+str(len(x_test))+" samples: ")
 print(x_test)
 print()        
-
-y_test=x_test.dot(w.T)
+'''
+y_test=np.dot(x_test,w)
 print("TEST SET Y values: with "+str(len(y_test))+" samples")
-print(y_test)    
-    
+print(y_test)     
+
+
+#RMSE = mean_squared_error(y_train, np.dot(x_train,w))**0.5
+print("RMSE=")
+print(RMSE)
+print()
+
+print("Lambda=")
+print(lam_best)
+print()
+
+
 #Write to an output file
 with open('banana.csv', 'wt') as csvfile:
     writer = csv.writer(csvfile)
